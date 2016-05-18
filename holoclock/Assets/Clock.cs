c1388febeb681304a71c6e6ea8a1ce344c618c50
islancoder <suzanna.kovoor@gmail.com>
2016-05-18 13:00:46 -0700

    clock hologram

---
+++ holoclock/Assets/Clock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Clock : MonoBehaviour {
+        
+
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+
+    public bool analog;
+
+    private const float hoursFactor= 360f / 12f, minutesFactor = 360f / 60f, secondsFactor = 360f / 60f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (analog)
+        {
+            TimeSpan t = DateTime.Now.TimeOfDay;
+            hoursTransform.localRotation = Quaternion.Euler(0f, 0f, - hoursFactor * (float) t.TotalHours);
+            minutesTransform.localRotation = Quaternion.Euler(0f, 0f, -minutesFactor * (float) t.TotalMinutes);
+            secondsTransform.localRotation = Quaternion.Euler(0f, 0f, -secondsFactor * (float) t.TotalSeconds);
+            Debug.Log((float)t.Minutes  );
+
+        }
+
+	
+	}
+}
