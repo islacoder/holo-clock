2306178eed4b3f56d78c34d8be87aba88df282ab
islancoder <suzanna.kovoor@gmail.com>
2016-05-18 22:22:55 -0700

    added cursor, gaze gesture recognition, spatial mapping and placing

---
+++ holoclock/Assets/parent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
